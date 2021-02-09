be3df604bbf9f226f70e96e4efa00d0d7c290d6c
DESKTOP-LSPDP1F\Thinh <thinh.do@fdssoft.com>
2021-02-09 23:20:55 +0700

    change form khuyen mai them,xoa,sua load form them status

--- 201_frmKhuyenMai.Designer.cs
+++ 201_frmKhuyenMai.Designer.cs
@@ -37,14 +37,15 @@ namespace Project
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
             this.grbText = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.txtTrangThai = new System.Windows.Forms.Label();
+            this.lblIdKhuyenMai = new System.Windows.Forms.Label();
             this.txtIdDiscount = new System.Windows.Forms.TextBox();
             this.txtMoneyDiscount = new System.Windows.Forms.TextBox();
-            this.txtPercentDiscount = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblTien = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtNameDiscount = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             btnDon = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgrKhuyenMai)).BeginInit();
             this.grbChucNang.SuspendLayout();
@@ -59,20 +60,22 @@ namespace Project
             btnDon.TabIndex = 3;
             btnDon.Text = "Dọn";
             btnDon.UseVisualStyleBackColor = true;
+            btnDon.Click += new System.EventHandler(this.btnDon_Click);
             // 
             // dtgrKhuyenMai
             // 
             this.dtgrKhuyenMai.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgrKhuyenMai.Location = new System.Drawing.Point(31, 272);
+            this.dtgrKhuyenMai.Location = new System.Drawing.Point(31, 321);
             this.dtgrKhuyenMai.Name = "dtgrKhuyenMai";
-            this.dtgrKhuyenMai.Size = new System.Drawing.Size(944, 284);
+            this.dtgrKhuyenMai.Size = new System.Drawing.Size(727, 284);
             this.dtgrKhuyenMai.TabIndex = 0;
             this.dtgrKhuyenMai.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrKhuyenMai_CellClick);
+            this.dtgrKhuyenMai.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrKhuyenMai_CellContentClick);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(304, 22);
+            this.label1.Location = new System.Drawing.Point(247, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(351, 46);
             this.label1.TabIndex = 1;
@@ -86,7 +89,7 @@ namespace Project
             this.grbChucNang.Controls.Add(this.btnThem);
             this.grbChucNang.Location = new System.Drawing.Point(490, 90);
             this.grbChucNang.Name = "grbChucNang";
-            this.grbChucNang.Size = new System.Drawing.Size(268, 176);
+            this.grbChucNang.Size = new System.Drawing.Size(268, 213);
             this.grbChucNang.TabIndex = 2;
             this.grbChucNang.TabStop = false;
             this.grbChucNang.Text = "Chức Năng";
@@ -100,6 +103,7 @@ namespace Project
             this.btnSua.TabIndex = 2;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnXoa
             // 
@@ -110,6 +114,7 @@ namespace Project
             this.btnXoa.TabIndex = 1;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnThem
             // 
@@ -119,35 +124,47 @@ namespace Project
             this.btnThem.TabIndex = 0;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // grbText
             // 
-            this.grbText.Controls.Add(this.label5);
+            this.grbText.Controls.Add(this.checkBox2);
+            this.grbText.Controls.Add(this.checkBox1);
+            this.grbText.Controls.Add(this.txtTrangThai);
+            this.grbText.Controls.Add(this.lblIdKhuyenMai);
             this.grbText.Controls.Add(this.txtIdDiscount);
             this.grbText.Controls.Add(this.txtMoneyDiscount);
-            this.grbText.Controls.Add(this.txtPercentDiscount);
-            this.grbText.Controls.Add(this.label4);
-            this.grbText.Controls.Add(this.label3);
+            this.grbText.Controls.Add(this.lblTien);
             this.grbText.Controls.Add(this.label2);
             this.grbText.Controls.Add(this.txtNameDiscount);
             this.grbText.Location = new System.Drawing.Point(31, 90);
             this.grbText.Name = "grbText";
-            this.grbText.Size = new System.Drawing.Size(418, 176);
+            this.grbText.Size = new System.Drawing.Size(418, 213);
             this.grbText.TabIndex = 3;
             this.grbText.TabStop = false;
             // 
-            // label5
+            // txtTrangThai
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(37, 22);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "idDisCount";
+            this.txtTrangThai.AutoSize = true;
+            this.txtTrangThai.Location = new System.Drawing.Point(10, 143);
+            this.txtTrangThai.Name = "txtTrangThai";
+            this.txtTrangThai.Size = new System.Drawing.Size(59, 13);
+            this.txtTrangThai.TabIndex = 8;
+            this.txtTrangThai.Text = "Trạng Thái";
+            // 
+            // lblIdKhuyenMai
+            // 
+            this.lblIdKhuyenMai.AutoSize = true;
+            this.lblIdKhuyenMai.Location = new System.Drawing.Point(11, 22);
+            this.lblIdKhuyenMai.Name = "lblIdKhuyenMai";
+            this.lblIdKhuyenMai.Size = new System.Drawing.Size(77, 13);
+            this.lblIdKhuyenMai.TabIndex = 7;
+            this.lblIdKhuyenMai.Text = "ID Khuyến Mãi";
+            this.lblIdKhuyenMai.Click += new System.EventHandler(this.label5_Click);
             // 
             // txtIdDiscount
             // 
-            this.txtIdDiscount.Location = new System.Drawing.Point(121, 19);
+            this.txtIdDiscount.Location = new System.Drawing.Point(131, 19);
             this.txtIdDiscount.Name = "txtIdDiscount";
             this.txtIdDiscount.Size = new System.Drawing.Size(173, 20);
             this.txtIdDiscount.TabIndex = 6;
@@ -155,58 +172,65 @@ namespace Project
             // 
             // txtMoneyDiscount
             // 
-            this.txtMoneyDiscount.Location = new System.Drawing.Point(121, 141);
+            this.txtMoneyDiscount.Location = new System.Drawing.Point(131, 95);
             this.txtMoneyDiscount.Name = "txtMoneyDiscount";
             this.txtMoneyDiscount.Size = new System.Drawing.Size(173, 20);
             this.txtMoneyDiscount.TabIndex = 5;
             // 
-            // txtPercentDiscount
+            // lblTien
             // 
-            this.txtPercentDiscount.Location = new System.Drawing.Point(121, 99);
-            this.txtPercentDiscount.Name = "txtPercentDiscount";
-            this.txtPercentDiscount.Size = new System.Drawing.Size(173, 20);
-            this.txtPercentDiscount.TabIndex = 4;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 144);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "MoneyDiscount";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 106);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "PercentDiscount";
+            this.lblTien.AutoSize = true;
+            this.lblTien.Location = new System.Drawing.Point(10, 102);
+            this.lblTien.Name = "lblTien";
+            this.lblTien.Size = new System.Drawing.Size(55, 13);
+            this.lblTien.TabIndex = 3;
+            this.lblTien.Text = "Tiền Giảm";
+            this.lblTien.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 61);
+            this.label2.Location = new System.Drawing.Point(10, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(115, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "NameDisCount";
+            this.label2.Text = "Sản Phẩm Khuyến Mãi";
             // 
             // txtNameDiscount
             // 
-            this.txtNameDiscount.Location = new System.Drawing.Point(121, 58);
+            this.txtNameDiscount.Location = new System.Drawing.Point(131, 58);
             this.txtNameDiscount.Name = "txtNameDiscount";
             this.txtNameDiscount.Size = new System.Drawing.Size(173, 20);
             this.txtNameDiscount.TabIndex = 0;
             this.txtNameDiscount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(131, 143);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "on";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(190, 143);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(38, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "off";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
             // KhuyenMai
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 648);
+            this.ClientSize = new System.Drawing.Size(786, 648);
             this.Controls.Add(this.grbText);
             this.Controls.Add(this.grbChucNang);
             this.Controls.Add(this.label1);
@@ -233,11 +257,12 @@ namespace Project
         private System.Windows.Forms.GroupBox grbText;
         private System.Windows.Forms.TextBox txtNameDiscount;
         private System.Windows.Forms.TextBox txtMoneyDiscount;
-        private System.Windows.Forms.TextBox txtPercentDiscount;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTien;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblIdKhuyenMai;
         private System.Windows.Forms.TextBox txtIdDiscount;
+        private System.Windows.Forms.Label txtTrangThai;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
